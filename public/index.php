63553faceac50f36982e91faffedde1cae5cc338
Lee Howard <git@leehoward.dev>
2023-03-15 19:26:48 -0700

    change content layout
    - move content from include into index.php
    
    - add close to final layout

--- public/index.php
+++ public/index.php
@@ -1,11 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php include '../includes/head.php'; ?>
 </head>
+
 <body>
     <?php include '../includes/header.php'; ?>
-    <?php include '../includes/content.php'; ?>
+
+    <section>
+        <h3>🧪 Projects</h3>
+        <ul>
+            <li>
+                <a href="https://github.com/lee0x1/fc" target="_blank">fc - github user first commit finder</a>
+        </ul>
+    </section>
+    <br>
+
+    <section>
+        <h3>📶 Contact / links</h3>
+        <ul>
+            <li>email: <a href="mailto:info@lee0x1.net">info@lee0x1.net</a></li>
+            <li>twitter: <a href="https://twitter.com/lee0x1/" target="_blank">@lee0x1</a></li>
+            <li>github: <a href="https://github.com/lee0x1" target="_blank">@lee0x1</a></li>
+        </ul>
+        <p>I'm usually very quick to respond via <a href="mailto:info@lee0x1.net">email</a>, but if<br>
+            you prefer to use social media hit me up on
+            <a href="https://twitter.com/lee0x1/" target="_blank">twitter</a>.
+        </p>
+        <p><em>Thank you for visiting.</em></p>
+    </section>
+    <br>
+    <hr>
+
+    <section>
+
+        <!-- TODO: php script to add articles to index using loop -->
+        <!-- 1. infinite loop: if .html file is added... -->
+        <!-- 2. add .html file into public folder -->
+        <!-- 3. add .html link: ul li a element -->
+
+        <h3>📔 Articles</h3>
+        <ul>
+            <li>03/16/23: <a href="back-to-the-future-wd.php">
+                    <strong>Back to the future of web development</strong>
+                </a>
+            </li>
+        </ul>
+    </section>
+    <br>
     <?php include '../includes/footer.php'; ?>
+
 </body>
+
 </html>
