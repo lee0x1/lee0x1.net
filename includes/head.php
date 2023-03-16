4022dc11ba63ea596463bb561fdc748eaa838cde
Lee Howard <git@leehoward.dev>
2023-03-15 17:36:28 -0700

    add initial styling
    - style tag from nginx default index.html
    
    - use monospace font

--- includes/head.php
+++ includes/head.php
@@ -2,3 +2,12 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Lee Howard's Website</title>
+<!-- TODO: spilt style from code -->
+<style>
+    body {
+        max-width: 35rem;
+        margin: 0 auto;
+        padding: 0 16px;
+        font-family: monospace; /* TODO: use custom font face? */
+    }
+</style>
