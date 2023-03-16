0d71518b0d221d8139e7dba3f7044c8601928476
leehowardx <git@leehoward.dev>
2023-03-16 14:07:15 -0700

    move articles code to includes folder

--- public/index.php
+++ public/index.php
@@ -39,27 +39,7 @@
     <section>
         <h3>📔 Articles</h3>
         <ul>
-            <?php
-            
-            $directories = glob('*', GLOB_ONLYDIR);
-            // search directory for article subdirectories
-            foreach ($directories as $dir) {
-                $phpfiles = glob($dir . "/index.php");
-                // search for index.html file
-                foreach ($phpfiles as $phpfile) {
-                    // parse html as string
-                    $str = file_get_contents($phpfile);
-                    // return tags string value
-                    $start = strpos($str, '<time>');
-                    $end = strpos($str, '</time>', $start);
-                    $date = substr($str, $start, $end-$start+7);
-                    // remove dashes from title name
-                    $title = str_replace('-', ' ', $dir);
-                    // add title as link tag to index page
-                    echo "<li>" . $date . ": <a href=\"$dir\">" . basename($title) . "</a></li>\n";
-                }
-            }
-            ?>
+            <?php include '../includes/articles.php'?>
         </ul>
     </section>
     <br>
