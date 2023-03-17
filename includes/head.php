0c1d34a85990461c5308003f7b92d5761fbc8a53
leehowardx <git@leehoward.dev>
2023-03-17 14:42:02 -0700

    create _assets folder
    - move css into folder

--- includes/head.php
+++ includes/head.php
@@ -2,5 +2,5 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?php echo $DESC; ?>">
-<link rel="stylesheet" href="<?php echo $ROOT; ?>css/style.css">
+<link rel="stylesheet" href="<?php echo $ROOT; ?>_assets/css/style.css">
 <title><?php echo $TITLE; ?></title>
