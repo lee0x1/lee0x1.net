ef028787d4098f2ce5e17f5a7ed8ba748f6e3166
Lee Howard <git@leehoward.dev>
2023-03-18 20:33:05 -0700

    add link and style to site title
    - link using $ROOT

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
 <header>
-    <h1>Lee Howard</h1>
+    <h1><a id="site-title" href="<?php echo $ROOT ?>">Lee Howard</a></h1>
     <p>Welcome to my personal network of development and other cool things.</p>
 </header>
