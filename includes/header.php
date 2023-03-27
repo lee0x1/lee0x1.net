2a7495fac2da2a5f7b551218a7f7f65fbc039ac8
leehowardx <git@leehoward.dev>
2023-03-27 13:01:04 -0700

    add conditional rendering of subtitle
    - if page is article dont show subtitle

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,11 @@
 <header>
     <h1><a id="site-title" href="<?php echo $ROOT ?>">Lee Howard</a></h1>
-    <p>Welcome to my personal network of development and other cool things.</p>
+    <?php
+    if (
+        $_SERVER['REQUEST_URI'] == '/lee0x1.net/' ||
+        $_SERVER['REQUEST_URI'] == '/lee0x1.net/public/'
+    ) {
+        echo "<p>Welcome to my personal network of development and other cool things.</p>";
+    }
+    ?>
 </header>
