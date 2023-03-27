798208e4e8dcec2de1cfb3aa6cc8cf8bbb631287
Lee Howard <git@leehoward.dev>
2023-03-26 17:58:55 -0700

    add accurate sorting using array of articles
    - add subdirectories with index.php to articles array
    
    - sort array oldest last
    
    - remove dashes from titles
    
    - echo li's

--- includes/articles.php
+++ includes/articles.php
@@ -3,26 +3,43 @@
 // all subdirectories in public folder (i.e: articles)
 $directories = glob('*', GLOB_ONLYDIR);
 
-// sort directories by dir creation date
-usort($directories, fn ($a, $b) => - (filemtime($a) - filemtime($b)));
-
 // search directory for article subdirectories
 foreach ($directories as $dir) {
 
     $phpfiles = glob($dir . "/index.php");
     // search for index.html file
     foreach ($phpfiles as $phpfile) {
-    // parse html as string
-    $str = file_get_contents($phpfile);
-    // return tags string value
-    $start = strpos($str, '<time>');
-    $end = strpos($str, '</time>', $start);
-    $date = substr($str, $start, $end-$start+7);
-    // remove dashes from title name
-    $title = str_replace('-', ' ', $dir);
-    // add title as link tag to index page
-    echo "<li>" . $date . ": <a href=\"$dir\">" . basename($title) . "</a></li>\n";
+        // parse html as string
+        $str = file_get_contents($phpfile);
+        // return tags string value
+        $start = strpos($str, '<time>');
+        $end = strpos($str, '</time>', $start);
+        $date = substr($str, $start, $end-$start+7);
+
+        // add each article to articles array as associative arrays
+        $articles[] = array("title" => $dir, "date" => $date);
     }
+}
+
+// sort array of arrays based on 'date' key=>val = title=>date
+usort($articles, function ($a, $b) {
+    return $b['date'] <=> $a['date'];
+});
+
+// iterate though articles 
+foreach ($articles as $article) {
+    
+    // removes dashes from article title
+    $title = str_replace('-', ' ', $article['title']);
+    
+    // add each article as list item
+    echo "<li>" .
+    $article['date'] .
+    ": <a href=" .
+    $article['title'] .
+    "><strong>" .
+    basename($title) .
+    "</strong></a></li>\n";
 }
 
 ?>
