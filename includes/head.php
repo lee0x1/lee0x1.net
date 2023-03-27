2e4f4db8ee71c5c32779d145c4efa0b2698e0532
leehowardx <git@leehoward.dev>
2023-03-27 10:35:26 -0700

    add author meta tag in head

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" content="Lee Howard">
 <meta name="description" content="<?php echo $DESC; ?>">
 <link rel="stylesheet" href="<?php echo $ROOT; ?>_assets/css/style.css">
 <title><?php echo $TITLE; ?></title>
